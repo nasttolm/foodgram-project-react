290cc2be9b05f1af310527fb095bd6e5c32ec918
nasttolm <nasttolm@gmail.com>
2023-10-24 17:53:47 +0100

    RecipeAdmin

--- foodgram/recipes/admin.py
+++ foodgram/recipes/admin.py
@@ -2,17 +2,18 @@ from django.contrib import admin
 
 from .models import Recipe, Tag, Ingredient, ShoppingCart, Favorite
 
-admin.site.register(Recipe)
 admin.site.register(Tag)
 admin.site.register(ShoppingCart)
 admin.site.register(Favorite)
 
 
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'image', 'description', 'ingredients',
                     'time', 'pub_date', 'author')
     search_fields = ('name', 'time')
-    list_filter = ('tag',)
+    list_filter = ('name', 'author', 'tags')
+    readonly_fields = ('in_favorites',)
     empty_value_display = '-пусто-'
 
 
